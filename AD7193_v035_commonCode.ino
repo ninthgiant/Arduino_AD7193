be099b78b339968c043556aabe9a862b0cecc2a1
ninthgiant <mauckr@kenyon.edu>
2024-01-19 16:19:29 -0500

    Update for Tacuna PCB

--- AD7193_v035_commonCode.ino
+++ AD7193_v035_commonCode.ino
@@ -1,85 +1,93 @@
 /*
- *   AD7193_SD_RTC_v03
- *   RAM, Sept. 1, 2023  
- * 
- *   Originally built from AD7193_VoltageMeasure_Example
- *   See my AD7193_Test (AD_1793_Test - actual name) for details of where this came from
- *
- *   NOTE: default pin assignment for PMOD AD7193 board MISO =10
- *          -- I changed it to 2 because SD shield uses PIN 10
- *            - that change is in the header file for AD7193 library; not in this file
- *   
- *   Sept. 1, 2023  
- *   - Add SD card to the app
- *   - setting file name using Date each time
- *   - Saving a time stamp each time slows it way down, so only save a counter
- *   - Saves data at about 68-70 samples per second;
- *   
- *   Dec 15, 2023
- *  - update so that it shares as much as possible with HX711 code - all but amplifier-specific code is common
- *  - use unsigned longs for data - simplifies life on back end
- *  - can't make AD9173 library work yet with Rev4 so this is ONLY for Rev3 UNO
- *
- *  On R3 Compile:
- *    Sketch uses 19478 bytes (60%) of program storage space. Maximum is 32256 bytes.
-*     Global variables use 1673 bytes (81%) of dynamic memory, leaving 375 bytes for local variables. Maximum is 2048 bytes.
+    FILE: AD7193_v05_commonCode
+    AUTHOR: RAM
+    DATE: 01/18/2024
+    PURPOSE: Handle load cell data collection using the AD7193 ADC on Tacuna PCB, PCB also has HX711, so this should work for both
+    STATUS: 
+
+    Based on HX711_R3_R4 code with Tacuna demo code in place of ADC-specific instructions
+    - from AD7193_Tacuna_v01 but stripping anything that might slow it down
+
+    this version has working components hi-speed looping while writing to open file
+    - to change: clean so doesn't use as much space
+    - make saving data faster by only accessing RTC once per loop, put that time stamp on every one in that loop - needed?
+    - set the rtc to the computer time - uses RTCLib (not RTC)
+
+    - Added AD7193 code from Tacuna and used library PRDC_AD7193 instead of AD7193 library
+
 */
 
-#include <Wire.h> 
+// libraries needed
 
-// headers for LCD
-#include <LiquidCrystal_I2C.h>
-// declare the lcd
-LiquidCrystal_I2C lcd(0x27, 20, 4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-
+#include <PRDC_AD7193.h>
+#include <LiquidCrystal.h>
+#include <SD.h>
 #include <SPI.h>
-#include <SD.h>  // interface with the SD card
-String myFilename;  // for saving data
+#include "Time.h"
+#include "RTClib.h"
 
-// #include <Time.h> // dealing with time stamps
-#include "RTClib.h"  // this is the Jeelabs version through Adafruit
-#include <TimeLib.h>;
-RTC_DS1307 RTC;  // instantiate a real time clock
+// Define variables
+// --------------------
+File myFile;
+String myFilename;
+
+// PCB variable defined by Tacuna code
+#define SRAM_CS 1 //Use A0 for Uno R3.  Use 1 for Uno R4
+#define SD_CS 10
+#define AD7193_CS 0 //Use A1 for Uno R3. Use 0 for Uno R4
+
+// Handle the ADC PCB unit
+// PRDC_AD7193 AD7193;
+
+// RTC
+RTC_DS1307 RTC;
 
 // Setup time variables that will be used in multiple places
 DateTime currenttime;
 long int myUnixTime;
 
-// AD7193 library - doesn't yet work with R4
-#include <AD7193.h>
-AD7193 scale;
-long int strain;
+// LCD instantiation
+const int rs = 9, en = 8, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-unsigned long tCounter = 0;  // Count # loops we to thru - must be global because don't want to initialize each time
+// AD7193 ADC scale
+PRDC_AD7193 scale;
+long int strain;  // value of the scale at any point in time
 
-// set constants
+int tCounter = 0;  // Count # loops we to thru - must be global because don't want to initialize each time
+
+/////////////////////
+//  set constants
+/////////////////////
+// set the samples to average when getting data
+const int myAVG = 80;
+
 // initialize variables for SD -- use chipSelect = 4 without RTC board
 const int chipSelect = 10;  // for the Wigoneer board and Adafruit board
+
 // set the communications speed
 const int comLevel = 115200;
-// set the delay amount
-const int dataDelay = 0;
+
 // set flag for amount of feedback - false means to give us too much info
 const bool verbose = true;
-// set flag for printing to screen - could automate this when check for serial
-const bool printScreen = true;
-// set flag for saving to disk
-const bool saveData = true;
+
 // set flag for printing to LCD
 const bool printLCD = true;
-// set flag to use the RTC
-const bool useRTC = true;
-// set flag to use the RTC
-const bool debug = false;
-// set flag to use the RTC
-const bool addStamp = true;
+
+// set flag to print somethnig only if debugging
+const bool debug = true;
+
+// flag for countdown
+const bool countdown = true;
+
 
 
 ////////////////////
-//  Get_Data - encapsulate data access to test different ideas - for now, very simple
+//  Get_Data - encapsulate data access to test different ideas - for now, very simple - would it be faster if we didn't use it at all?
 ////
-unsigned long Get_Data() {
-  return (scale.ReadADCChannel(1));
+long int Get_Data() {
+  // return (scale.read());
+  return scale.continuousReadAverage(myAVG);
 }
 
 ////////////////////
@@ -88,11 +96,18 @@ unsigned long Get_Data() {
 long int Get_TimeStamp() {
   /* GET CURRENT TIME FROM RTC */
   currenttime = RTC.now();
-
   // convert to raw unix value for return - could give options
   return (currenttime.unixtime());
 }
 
+////////////////////
+//  Get_TimeStampString - encapsulate data in case we change the way we record time
+////
+String Get_TimeStampString() {
+  /* GET CURRENT TIME FROM RTC */
+  currenttime = RTC.now();
+  return(currenttime.timestamp(DateTime::TIMESTAMP_TIME));
+}
 
 ///////////////////
 // File name function - based on RTC time - so that we have a new filename for every day "DL_MM_DD.txt"
@@ -115,164 +130,182 @@ String rtnFilename() {
 }
 
 
-
 void setup() {
 
-  ///////////////////////////
-  // setup Serial and SPI
-  ///////////////////////////
+  ////////// 
+  // Set CS pins high as soon as we can - from TACUNA
+  ////////
+  pinMode(SRAM_CS, OUTPUT); 
+  digitalWrite(SRAM_CS, HIGH);
 
+  pinMode(SD_CS, OUTPUT); 
+  digitalWrite(SD_CS, HIGH);
+
+  pinMode(AD7193_CS, OUTPUT); 
+  digitalWrite(AD7193_CS, HIGH);
+
+  // Communication settings
   Serial.begin(115200);
-
-  while (!Serial) {
-    ;  // wait for serial port to connect. Needed for native USB port only
-  }
+  Serial.println("setup lcd");
 
   ///////////////////////////
   // setup LCD
   ///////////////////////////
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("Checking...");  // remove this when actually checking
+    // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+  lcd.setCursor(0,0);     // user feedback in the field
+  lcd.print("Checking...");  
   delay(1000);
   lcd.setCursor(0,0);
-  lcd.print("                ");  // remove this when actually checking
+  lcd.print("                "); 
 
-  //
   ///////////////////////////
-  // RTC - turn on and off with flag
+  // setup ADC AD7193 on PCB from Tacuna code
   ///////////////////////////
+  scale.setSPI(SPI);
+    if(!scale.begin(AD7193_CS, PIN_SPI_MISO)) {
+      Serial.println(F("AD7193 initialization failed!"));
+    } else {
+      scale.printAllRegisters();
+      scale.setClockMode(AD7193_CLK_INT);
+      scale.setRate(0x001);
+      scale.setFilter(AD7193_MODE_SINC4);
+      scale.enableNotchFilter(false);     // learn what this will do
+      scale.enableChop(false);
+      scale.enableBuffer(true);
+      scale.rangeSetup(0, AD7193_CONF_GAIN_128);
+      scale.channelSelect(AD7193_CH_0);
+      Serial.println(F("AD7193 Initialized!"));
+    }
+
 
   ///////////////////////////
   // RTC - Setup - turn on and off with flag
   ///////////////////////////
-  if (useRTC) {  // must do this first so that we can use the GetFileName when we want it
 
-    Serial.println("RTC setup");
-    if (!RTC.begin()) {  // initialize RTC
-      Serial.println("RTC failed");
+  Serial.println("RTC setup");
+  if (!RTC.begin()) {  // initialize RTC
+    Serial.println("RTC failed");
+  } else {
+    if (!RTC.isrunning()) {
+      Serial.println("RTC is NOT running!");
+      // following line sets the RTC to the date & time this sketch was compiled
+      RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
     } else {
-      if (!RTC.isrunning()) {
-        Serial.println("RTC is NOT running!");
-        // following line sets the RTC to the date & time this sketch was compiled
-        RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
-      } else {
-        RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
-        Serial.println("RTC is already initialized.");
-      }
+      RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
+      Serial.println("RTC is already initialized. Time: ");
+      Serial.println(String("DateTime::TIMESTAMP_FULL:\t")+RTC.now().timestamp(DateTime::TIMESTAMP_FULL));
     }
-    // need to check to see if the clock is correct, or always update to internal clock
   }
+
 
   ///////////////////////////
   // setup SD Card - turn on and off with flag
   ///////////////////////////
-  if (saveData) {
 
-    if (debug) {
-      myFilename = "Test_013.txt";  // use this if you want a custom name
-      // myFilename = rtnFilename();  // use this if debugging and want to have the autonamed file - NEED RTC running to make it work
+
+  if (debug) {
+    myFilename = "Test_prt.txt";  // use this if you want a custom name
+    // myFilename = rtnFilename();  // use this if debugging and want to have the autonamed file - NEED RTC running to make it work
+  } else {
+    myFilename = rtnFilename();
+  }
+
+  Serial.print("Saving to: ");
+  Serial.println(myFilename);
+
+  // setup lcd for user feedback
+  lcd.setCursor(0, 0);
+
+  // initialize the SD card process with user feedback
+  Serial.print("Initializing SD card...");
+
+  delay(1000);
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");  // don't do anything more:
+    lcd.print("                ");
+    lcd.setCursor(0, 0);
+    lcd.print("Card failed!");
+    lcd.setCursor(1, 1);
+    lcd.print("Disconnect!     ");
+    delay(10000);
+
+    while (1)
+      ;
+    Serial.println("card initialized.");  // confirm that it is good to go
+  }                                       // end of checking for card and initializing
+
+
+
+  if (verbose) {  // give full feedback on status to the user
+
+    float wt_Check01;  // track current weight
+    float wt_Check02;  // track the raw values from load cell
+    float wt_Check03;  // track the raw values from load cell
+
+    Serial.println("Before setting up the scale:");
+    Serial.print("read: \t\t\t");
+    Serial.println(scale.singleConversion());  // print a raw reading from the ADC
+
+    Serial.print("read average: \t\t");
+    Serial.println(Get_Data());  // print the average of normal sample of readings from the ADC
+
+    wt_Check01 = scale.singleConversion();
+    Serial.print("Check 01: \t\t");
+    Serial.println(wt_Check01);
+    delay(200);
+    wt_Check02 = scale.singleConversion();
+    Serial.print("Check 02: \t\t");
+    Serial.println(wt_Check02);
+    delay(200);
+    wt_Check03 = scale.singleConversion();
+    Serial.print("Check 03: \t\t");
+    Serial.println(wt_Check03);
+    delay(200);
+
+    if ((wt_Check01 == wt_Check02) & (wt_Check02 == wt_Check03)) {
+      Serial.print("We have a problem; load cell always reads: ");
+      Serial.println(wt_Check01);
+      lcd.setCursor(1, 0);
+      lcd.print("Load cell Problem!");
+      lcd.setCursor(1, 1);
+      lcd.print("Disconnnect!    ");
+      delay(10000);
     } else {
-      myFilename = rtnFilename();
+      lcd.setCursor(0, 0);
+      lcd.print("Load cell works");
+      Serial.println("Load cell working properly.");
+      delay(1000);
     }
 
-    // setup lcd for user feedback
-    lcd.setCursor(0, 0);
-    lcd.print("Initialize card ");
-
-    // initialize the SD card process
-    Serial.print("Initializing SD card...");
-
-    delay(1000);
-
-    // see if the card is present and can be initialized:
-    if (!SD.begin(chipSelect)) {
-      Serial.println("Card failed, or not present");  // don't do anything more:
-      lcd.print("                ");
-      lcd.setCursor(1, 0);
-      lcd.print("Card failed!");
-      lcd.setCursor(1, 1);
-      lcd.print("Disconnect!");
-      delay(10000);
-      // turn off the lcd
-      lcd.noBacklight();
-      lcd.noDisplay();
-
-
-      while (1)
-        ;
-      Serial.println("card initialized.");  // confirm that it is good to go
-    }                                       // end of checking for card and initializing
-
-  }  // end of if SaveDATA
-
-
-
-  ///////////////////////////////////
-  // AD7193 Device setup
-  ///////////////////////////////////
-
-  scale.begin();
-
-  //This will append status bits onto end of data - is required for library to work properly
-  scale.AppendStatusValuetoData();
-
-  // Set the gain of the PGA
-  scale.SetPGAGain(128);
-
-  // Set the Averaging
-  scale.SetAveraging(3);  // was 3 originally - higher number slower sps
-
-
-  //////////////////////////////////////
-  // Inform the user of current status after checking if AD7193 is working - similar, but different from HX711 code
-  /////////////////////////////////////
-
-  bool lcGood = true;  // set flag
-  // Check that Load Cell is working properly
-  if (Get_Data() == Get_Data() == Get_Data()) {  //  all the same so is wrong
-    Serial.println("Bad data");
-    lcd.setCursor(0, 0);
-    lcd.print("Load cell BAD! ");
-    lcGood = false;
-    delay(10000);
-  } else {
-    lcd.setCursor(0, 0);
-    lcd.print("Load cell works");
-    delay(1000);
-
-    Serial.println("Load cell working properly.");
-
+  if(countdown){
     int N = 10;
     for (int i = 1; i < N; i++) {
-
       lcd.setCursor(0, 1);
       lcd.print("Start in: ");
       lcd.print(N - i);
-      lcd.print(" s");
-
-      delay(1000);
-    }  // end of for loop
-
-    lcd.setCursor(0, 1);
-    lcd.print("Data:           ");
-
+      lcd.print(" secs");
+      delay(800);
+    }
+  }
     
-  }    // end of if lcGood, else
+  }
 
   // turn off the lcd?
   if (!printLCD) {
-    lcd.noBacklight();
-    lcd.noDisplay();
+    // lcd.noBacklight();
+    // lcd.noDisplay();
   } else {
     lcd.setCursor(0, 0);
     lcd.print("Data:           ");
     lcd.setCursor(0, 1);
     lcd.print("                ");
+    lcd.setCursor(6, 0);
+    lcd.print(Get_Data());  // do this while we are messing with closing the datafile
   }
-
-
 }
 
 
@@ -281,56 +314,45 @@ void loop() {
   unsigned long strain;
 
     // get a single time stamp to put on all 300 data points - not sure worth the speed to lose the resolution
-  myUnixTime = Get_TimeStamp();
+  // myUnixTime = Get_TimeStamp();
+    myUnixTime = tCounter;
     // open the file to write 300 data points
   File dataFile = SD.open(myFilename, FILE_WRITE);
 
   if (dataFile) {
 
-    for (int i = 0; i < 300; i++) {
+    for (int i = 0; i < 10; i++) {
 
       strain = Get_Data();
-      
 
-      if (addStamp) {  // only adds it to the saved data
-        dataFile.print(strain);
-        dataFile.print(", ");  // do we want to add the time stamp
-        dataFile.println(myUnixTime);
-
-        // datafile.println(currenttime.getUnixTime());  // for now, do it every time. let's see how it goes
-      } else {
-        dataFile.print(strain);
-        dataFile.println(", ");  // do we want to add the time stamp
-      }
-
+      dataFile.print(strain);
+      dataFile.print(", ");  
+      dataFile.println(myUnixTime); // add the time stamp on every loop
 
       if (debug) {
-        Serial.print(strain);
-        if (i < 1) {
-          Serial.print(",");
-          Serial.println(tCounter);
-        } else {
-          Serial.print(",");
-          Serial.println(Get_TimeStamp());
-        }
 
-      } else {
-        Serial.println(strain);   //activate this only for testing wiht debug false
-        // Serial.println(",");
+        Serial.print(strain);
+        Serial.print(", ");  
+        // Serial.println(Get_TimeStampString()); // add the time stamp each time which will slow it down
+        Serial.println(myUnixTime); // add the time stamp on every loop with the UNIX TIME
+
       }  // end of if debug
 
 
 
-    }  // end of for loop; data file being open
+    }  // end of for loop; data file being open, now do time lag items
 
     // show user we are collecting data
+    // Write to LCD
     lcd.setCursor(6, 0);
     lcd.print(strain);  // do this while we are messing with closing the datafile
 
+    // close file, saving all in the loop
     dataFile.close();  // at the end, close before we start another loop
 
 
-  } else {
+  } else {  // if you can't open a file, we can't go forward. Show user that it didn't work
+
     Serial.println("Error opening file ");  //  + myFile);  // if the file isn't open, pop up an error:
     lcd.setCursor(0, 0);
     lcd.print("Can't open file!!");  // do this while we are messing with closing the datafile
@@ -338,8 +360,10 @@ void loop() {
     lcd.setCursor(0, 1);
     lcd.print("Figure it out!");  // do this while we are messing with closing the datafile
     delay(10000);
+
   }
 }
 
 
 // -- END OF FILE --
+
